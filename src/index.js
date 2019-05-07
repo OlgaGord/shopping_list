8c7e745b9ebf21898c4989f841f12740c9b06d6f
unknown <olga1gordeyeva@gmail.com>
2019-05-06 22:28:29 -0400

    create react components for list, header, search panel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const el = <h1>Hello World!</h1>;
+const ShoppingList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Learn C#</li>
+    </ul>
+  );
+};
+
+const AppHeader = () => {
+  return <h1>Shopping list</h1>;
+};
+
+const SearchBar = () => {
+  return <input placeholder="Search" />;
+};
+
+const el = (
+  <div>
+    <AppHeader />
+    <SearchBar />
+    <ShoppingList />
+  </div>
+);
 ReactDOM.render(el, document.getElementById("root"));

6ee62207da6be10354a37f204ab092933767c598
unknown <olga1gordeyeva@gmail.com>
2019-09-29 14:43:27 -0400

    test

--- src/components/shopping-list-item/shopping-list-item.js
+++ src/components/shopping-list-item/shopping-list-item.js
@@ -2,9 +2,25 @@ import React, { Component } from "react";
 import "./shopping-list-item.css";
 
 export default class ShoppingListItem extends Component {
+
+  state = {
+    done: false
+  };
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    });
+  };
+
   render() {
 
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let className = "shopping-list-item";
+    if (done) {
+      className += 'done';
+    }
 
     const style = {
       color: important ? "stealblue" : "black",
@@ -14,7 +30,9 @@ export default class ShoppingListItem extends Component {
 
       <span className="shopping-list-item">
         <span className="shopping-list-item-label"
-          style={style}>{label}</span>
+          style={style}
+          onClick={this.onLabelClick}
+        >{label}</span>
 
         <button type="button" className="btn btn-outline-success btn-sm float-right">
           <i className="fa fa-exclamation" />
